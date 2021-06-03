c332da93673f1509e1bff3afa8a440ebc2225e8c
Korneliu <olbiton@gmail.com>
2021-06-02 22:13:11 -0700

    corrected typo in AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
@@ -14,7 +14,7 @@ const AppRouter = () => (
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashBoardPage} exact={true} />
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
