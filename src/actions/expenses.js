23b2cd4467ce44e1f5f35cbff4381dbc23018868
Korneliu <olbiton@gmail.com>
2021-07-06 23:27:05 -0700

    added startAddExpense action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,24 +1,32 @@
 import { v4 as uuidv4 } from 'uuid';
+import database from '../firebase/firebase';
 
 
 // ADD_EXPENSE action generator functions
-export const addExpense = (
-  {
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
-  } = {}
-) => ({
+export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
-  expense: {
-    id: uuidv4(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 })
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+
+    const expense = { description, note, amount, createdAt };
+    database.ref('expenses').push(expense)
+      .then((ref) => {
+        dispatch(addExpense({
+          id: ref.id,
+          ...expense
+        }))
+      });
+  };
+};
 
 // REMOVE_EXPANSE
 export const removeExpense = ({ id } = {}) => ({
@@ -38,3 +46,4 @@ export const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
   text
 })
+
